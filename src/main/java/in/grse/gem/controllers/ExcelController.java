1d171b987c53b15b074839d083c3dc4bf9f2ac24
unknown <Wasif Ali@DESKTOP-HDDAJ88.grse.co.in>
2026-04-20 14:21:55 +0530

    added comment

--- src/main/java/in/grse/gem/controllers/ExcelController.java
+++ src/main/java/in/grse/gem/controllers/ExcelController.java
@@ -18,7 +18,7 @@ public class ExcelController {
      * Excel generate
      *
      * @param json data
-     * @return Pdf file
+     * @return Pdf file with all the required data
      * @throws Exception
      */
     @PostMapping("/generate")
