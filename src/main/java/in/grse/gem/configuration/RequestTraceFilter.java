aeec7e78f7ef8caf5def1447c978272be8082e50
unknown <Wasif Ali@DESKTOP-HDDAJ88.grse.co.in>
2026-04-21 12:19:15 +0530

    Added request traced filter

---
+++ src/main/java/in/grse/gem/configuration/RequestTraceFilter.java
@@ -0,0 +1,35 @@
+package in.grse.gem.configuration;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class RequestTraceFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String traceId = UUID.randomUUID().toString();
+
+        MDC.put("traceId", traceId);
+        MDC.put("path", request.getRequestURI());
+        MDC.put("method", request.getMethod());
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
